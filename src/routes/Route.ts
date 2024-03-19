20c23fd2e579145c4c01b64230c1e671c349e11e
ericvegax <mcpe2004salsa@gmail.com>
2024-03-19 16:42:41 -0400

    updated Route.ts

--- src/routes/Route.ts
+++ src/routes/Route.ts
@@ -10,7 +10,7 @@ class Route {
         this.router = express.Router();
     }
 
-    registerEvent(path: string, method: Method, call: (req: Request, res: Response) => Promise<any> | void) {
+    registerEvent(path: string, method: Method, call: (req: Request, res: Response) => void) {
         if (method === Method.GET) {
             this.router.get(path, (req, res) => call(req, res));
         } else {
